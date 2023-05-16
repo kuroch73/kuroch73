0e98d405317217fdbff9bcbbd290dd489e20d5a2
kuroch73 <kuroch73@yandex.ru>
2023-05-16 19:37:34 +0400

    lesson 2

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-int = 1
-float = 0.1
-string = "text"
-bool = True
+# a = 2
+# a= str(a)
+# print(str(a))
+# print(type(a))
+# a = float(a)
+# print(type(a))
+# print(a)
+
+# c = float(input("insert number - we multipy it on two: ")) # input is default string 
+# print(c *2)
+print("Fill info")
+myName = input ("Insert name ")
+age = int(input("Insert age "))
+hobby = input("Insert your hobby ")
+job = input("Insert your job ")
+
+
+print("User's Info: ")
+print("Name:" , myName )
+
+print("Age: ", age)
+print("Hobby: ", hobby )
+print("Job: ", job)
