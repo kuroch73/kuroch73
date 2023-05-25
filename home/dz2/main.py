ab7e91016526a46d2893303408b8c5d1afc9cc39
kuroch73 <kuroch73@yandex.ru>
2023-05-25 15:52:49 +0400

    dz2 done

---
+++ home/dz2/main.py
@@ -0,0 +1,30 @@
+# вывод по данным пользователя суммы или произведения
+# a = int(input("insert number  "))
+# b = int(input("insert number  "))
+# c = int(input("insert number  "))
+# d = int(input("choose 1 - сумма, 0 - произведение: "))
+# if d == 1:
+#     print(a + b + c)
+# else:
+#     print(a*b*c)
+# 
+# вывод по данным пользователя мин маки и среднего
+# a = int(input("insert number  "))
+# b = int(input("insert number  "))
+# c = int(input("insert number  "))
+# d = int(input("choose 1 - ммаксимум, 0 - минимум, 2 -  среднее: "))
+# if d == 1:
+#     print(max((a,b,c)))
+# elif d == 0:
+#     print(min((a,b,c)))
+# else:
+#     print((a +b + c)/2)
+# перевод метров в мили, дюймы, ярды
+# a = int(input("insert meters  "))
+# d = int(input("choose 1 - мили, 0 - дюймы, 2 -  ярды: "))
+# if d == 1:
+#      print("мили ",a*0.000621)
+# elif d == 0:
+#      print("дюймы ", a*39.37)
+# else:
+#      print("ярды ", a*1.09)
