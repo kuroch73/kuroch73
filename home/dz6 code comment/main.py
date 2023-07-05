8c4192ba23f84611387fed4bc40f1e5db0d1bdb0
kuroch73 <kuroch73@yandex.ru>
2023-07-05 22:21:16 +0400

    dz code comment done

--- home/dz6 code comment/main.py
+++ home/dz6 code comment/main.py
@@ -1,10 +1,11 @@
 numberList = [1,2,3,4,5,6,7,8,9,22,11,10,6,5,22]
-def delNumber(massiv) :
-    print("вошел", massiv , "количество" , len(massiv))
-    for i in range(0, len(massiv)):
-        if i == len(massiv):
-            return massiv
-        elif massiv[i] % 2 == 0 :
-            massiv.pop(i)
-            print("вышел", massiv)
-delNumber(numberList)
+def delNumber(massiv) : # функция delNumber с параметром massiv - список []
+    print("вошел", massiv , "количество" , len(massiv)) # вывод списка с количеством элементов
+    for i in range(0, len(massiv)): # цикл от 0 элемента до длины списка
+        if i == len(massiv): # если элемент равен длине списка
+            return massiv # 
+        elif massiv[i] % 2 == 0 : # также если элемент списка четный
+            massiv.pop(i) # удалить его из списка
+            print("вышел", massiv) # вывод "вышел" и получившийся список
+            delNumber(massiv) # вызов функции с выводом списка и количеством элементов
+delNumber(numberList) # вызов функции с параметром numberList  т.е. обработанном списком
